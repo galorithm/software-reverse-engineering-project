afe62c573bde805f298d4a2a7ee4279f0785cb6c
Eshan Kelkar <eshankelkar@galorithm.com>
2026-04-29 08:36:52 -0500

    Add procmon tool event capturing feature to the tool

--- additional_requirement_tool/src/ek_tool.py
+++ additional_requirement_tool/src/ek_tool.py
@@ -1,12 +1,18 @@
 '''
-Limitations:
+Limitations/Future TODOs
 1. Multiline strings are broken into different line in strings output even
    if the original string contained \n itself
+
+2. Adding support for CLI argument passing to malware file (right now its
+   executed with 0 cli args)
+
+3. Make duration to run dynamic (take from the user)
 '''
 import argparse
 import subprocess
+import time
 import pefile
-
+import csv
 import json
 import os
 import sys
@@ -89,6 +95,101 @@ def get_exports_from_pe_file(pe_file_path):
 
     return exports_list
 
+def get_event_list_from_procmon_output_csv_file(procmon_output_csv_file_path):
+    if not os.path.exists(procmon_output_csv_file_path):
+        raise ValueError(f'No procmon output csv file at {procmon_output_csv_file_path}!')
+
+    events_list = []
+
+    with open(procmon_output_csv_file_path, 'r', encoding = 'utf-8') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            events_list.append(
+                    {
+                        "time": row.get("Time of Day"),
+                        "process": row.get("Process Name"),
+                        "operation": row.get("Operation"),
+                        "path": row.get("Path"),
+                        "result": row.get("Result"), # success/failure
+                        "detail": row.get("Detail") # extra stuff
+                    })
+
+    return events_list
+
+def get_procmon_events_list_on_malware_execution(
+        procmon_executable_file_path,
+        procmon_config_file_path,
+        malware_file_path):
+    
+    if not os.path.exists(procmon_executable_file_path):
+        raise ValueError(f'procmon exectuable not found at : {procmon_executable_file_path}')
+
+    if not os.path.exists(procmon_config_file_path):
+        raise ValueError(f'procmon config file not found at: {procmon_config_file_path}')
+
+    procmon_output_log_file_path = "tmp_ek_tool_procmon_output_log_file.pml"
+    procmon_output_csv_file_path = "tmp_ek_tool_procmon_output_csv_file.csv"
+
+    # start Procmon capture in background (this starts it and returns)
+    logging.info('Starting procmon capture')
+    procmon_process = subprocess.Popen(
+            [procmon_executable_file_path,
+
+             # e.g. allows setting custom filters
+             "/LoadConfig", procmon_config_file_path,
+
+             # no UI window on launch
+             "/Quiet",
+             "/Minimized", 
+
+             # write/log events directly to this file instead of VRAM
+             "/BackingFile", procmon_output_log_file_path
+             ])
+
+    logging.info('Sleeping for 3s to let procmon start')
+    time.sleep(3)
+
+    # Start malware (but don't wait for it to end, hence Popen instead
+    # of run)
+    logging.info(f'Starting malware {malware_file_path}')
+    malware_process = subprocess.Popen(malware_file_path)
+
+    # log events for 10 seconds
+    logging.info(f'Sleeping for 10 seconds to capture events')
+    duration_to_wait = 10
+    time.sleep(duration_to_wait)
+
+    # if not already closed, notify the malware process to close
+    malware_process.terminate()
+
+    # Notify procmon to stop procmon capture
+    logging.info('Notifying procmon to stop capture')
+    subprocess.run(
+            [procmon_executable_file_path, "/Terminate", "/Quiet"],
+            check = True
+            )
+
+    # wait for it to close
+    procmon_process.wait()
+
+    # Convert procmon log file (custom format) to more  parseable
+    # csv format
+    logging.info('Converting procmon log file to csv format')
+    subprocess.run(
+            [procmon_executable_file_path,
+             "/OpenLog", procmon_output_log_file_path,
+             "/SaveAs", procmon_output_csv_file_path],
+            check = True
+            )
+
+    event_list = get_event_list_from_procmon_output_csv_file(procmon_output_csv_file_path)
+
+    # remove the tmp output files
+    os.remove(procmon_output_log_file_path)
+    os.remove(procmon_output_csv_file_path)
+
+    return event_list
+
 def parse_cli_args():
     """
     Parse command line arguments and return the args object (argparse)
@@ -115,8 +216,12 @@ def parse_cli_args():
     return parser.parse_args()
 
 CONFIG_KEY_STRINGS_EXECUTABLE_FILE_PATH = "STRINGS_EXECUTABLE_FILE_PATH"
+CONFIG_KEY_PROCMON_EXECUTABLE_FILE_PATH = "PROCMON_EXECUTABLE_FILE_PATH"
+CONFIG_KEY_PROCMON_CONFIG_FILE_PATH = "PROCMON_CONFIG_FILE_PATH"
 CONFIG_EXPECTED_KEYS = [
-        CONFIG_KEY_STRINGS_EXECUTABLE_FILE_PATH
+        CONFIG_KEY_STRINGS_EXECUTABLE_FILE_PATH,
+        CONFIG_KEY_PROCMON_EXECUTABLE_FILE_PATH,
+        CONFIG_KEY_PROCMON_CONFIG_FILE_PATH
         ]
 def parse_config_file(config_file_path):
     """
@@ -154,12 +259,14 @@ def dump_details_to_json(malware_file_path,
                          strings_list,
                          imports_dict,
                          exports_list,
+                         procmon_events_list,
                          output_json_file_path):
     output_json_dict = {
             "file_path": malware_file_path,
             "strings": strings_list,
             "imports": imports_dict,
             "exports": exports_list,
+            "procmon_events_list": procmon_events_list
             }
 
     with open(output_json_file_path, "w", encoding = "utf-8") as f:
@@ -174,14 +281,25 @@ if __name__ == "__main__":
     malware_file_path = args.input_malware_file_path
     output_json_file_path = args.output_json_file_path
 
-    strings_list = get_strings_from_pe_file(config[CONFIG_KEY_STRINGS_EXECUTABLE_FILE_PATH],
+    strings_executable_file_path = config[CONFIG_KEY_STRINGS_EXECUTABLE_FILE_PATH]
+    procmon_executable_file_path = config[CONFIG_KEY_PROCMON_EXECUTABLE_FILE_PATH]
+    procmon_config_file_path = config[CONFIG_KEY_PROCMON_CONFIG_FILE_PATH]
+
+    strings_list = get_strings_from_pe_file(strings_executable_file_path,
                                             malware_file_path)
 
     imports_dict = get_imports_from_pe_file(malware_file_path)
     exports_list = get_exports_from_pe_file(malware_file_path)
 
+    procmon_events_list = get_procmon_events_list_on_malware_execution(
+            procmon_executable_file_path,
+            procmon_config_file_path,
+            malware_file_path
+            )
+
     dump_details_to_json(malware_file_path,
                          strings_list,
                          imports_dict,
                          exports_list,
+                         procmon_events_list,
                          output_json_file_path)
